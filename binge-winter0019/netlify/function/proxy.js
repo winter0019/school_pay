2361db2cad726d6d674ac287a57bb76a3ff21afb
winter0019 <121707096+winter0019@users.noreply.github.com>
2025-10-08 08:51:35 +0100

    Delete binge-winter0019 directory

--- binge-winter0019/netlify/function/proxy.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  // Example proxy function. Use this to securely fetch data from an API if needed.
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Binge Winter0019 proxy!" })
-  };
-};
