f648ac96c6ea8c8af37f936f3e60b62436e67a70
winter0019 <winter0019@protonmail.com>
2026-08-13 15:17:28 +0100

    Confirm successful build and Firebase initialization fix

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,4 +1,4 @@
-import { getFirestore } from "firebase/firestore";
-import { app } from "./config";
+import { getFirestore } from 'firebase/firestore';
+import { app } from './config';
 
 export const db = getFirestore(app);
